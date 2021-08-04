2505a5e5c6b80969d671b2e0ec39e749d5dd1401
Ari Stehney <ari.stehney@icloud.com>
2021-08-04 17:00:09 -0600

    Fix build errors

--- src/components/YAMLConfiguration.vue
+++ src/components/YAMLConfiguration.vue
@@ -1,11 +1,8 @@
 <template>
   <v-container>
-    <v-row justify="space-around">
-      <br><br>
-      Nothing here yet, coming in a future version once YAML support is added.
-    </v-row>
-
-    
+      <!--<br><br>
+      Nothing here yet, coming in a future version once YAML support is added.-->
+      <v-treeview :items="optiontree"></v-treeview>
   </v-container>
 </template>
 
@@ -18,13 +15,39 @@ export default {
   
   data () {
     return {
-      
+      optiontree: []
     }
   },
 
   methods: {
     async updateConfig() {
-      
+      var data = YAML.parse(`---
+- ModelName: Test1
+  RXType: CRSF
+  ModelID: '3'
+- ModelName: Test2
+  RXType: SBUS
+  ModelID: '2'
+- ModelName: Test6
+  RXType: INT.
+  ModelID: '14'`);
+
+      var idxct = 1;
+
+      this.optiontree = data.map(function(obj){
+          idxct+=1;
+          var keys = Object.keys(obj);
+          return {
+            id: idxct,
+            children: keys.map(function(itm) {
+              idxct+=1;
+              return {id: idxct, name: `${itm}: ${obj[itm]}`}
+            }),
+            name: obj.ModelName
+          };
+        });
+
+      console.log(this.optiontree);
     },
   },
 
